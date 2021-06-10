002abf4b3d46652a3915a59b94467be4437957be
Alexandre Wery <alexbwery@gmail.com>
2021-06-10 05:35:06 +0200

    initial structure

---
+++ cmake/GnuplotCommand.cmake
@@ -0,0 +1,34 @@
+find_program(GNUPLOT_COMMAND gnuplot)
+
+define_property(TARGET PROPERTY PLOT_NAME BRIEF_DOCS "plot name" FULL_DOCS "plot name")
+define_property(TARGET PROPERTY PLOT_TYPE BRIEF_DOCS "plot type" FULL_DOCS "plot type")
+
+function(generate_plot target)
+    set(oneValueArgs OUTPUT FORMAT_AXIS FUNCTION XLABEL YLABEL)
+    set(manyValueArgs SOURCES)
+    cmake_parse_arguments(PLOT "" "${oneValueArgs}" "${manyValueArgs}" ${ARGN})
+    set(ARG "")
+    foreach(inTarget ${PLOT_SOURCES})
+        if(NOT "${ARG}" STREQUAL "")
+            string(APPEND ARG " ,")
+        endif()
+        string(APPEND ARG "'$<TARGET_PROPERTY:${inTarget},OUTPUT_NAME>' using ${PLOT_FORMAT_AXIS} with $<TARGET_PROPERTY:${inTarget},PLOT_TYPE> title '$<TARGET_PROPERTY:${inTarget},PLOT_NAME>'")
+    endforeach()
+
+    set(LABEL "")
+    if(NOT "${PLOT_XLABEL}" STREQUAL "")
+        string(APPEND LABEL "set xlabel '${PLOT_XLABEL}'; " )
+    endif()
+    if(NOT "${PLOT_YLABEL}" STREQUAL "")
+        string(APPEND LABEL "set ylabel '${PLOT_YLABEL}'; " )
+    endif()
+
+    add_custom_command(
+            OUTPUT ${PLOT_OUTPUT}
+            COMMAND ${GNUPLOT_COMMAND} -e "${LABEL}" -e "set term png; set output '${PLOT_OUTPUT}'; ${PLOT_FUNCTION}; plot ${ARG}"
+            DEPENDS ${PLOT_SOURCES}
+            VERBATIM
+    )
+    add_custom_target(${target} DEPENDS ${PLOT_OUTPUT})
+    set_property(TARGET ${target} PROPERTY OUTPUT_NAME "${PLOT_OUTPUT}")
+endfunction()
---
+++ cmake/nlomannJson.cmake
@@ -0,0 +1,20 @@
+find_package(nlohmann_json 3.2.0 QUIET)
+if (NOT nlohmann_json_FOUND)
+    set(NLOHMANN_JSON_OUTPUT ${CMAKE_SOURCE_DIR}/dependencies CACHE INTERNAL "Output directory of download")
+    set(NLOHMANN_JSON_DIR nlomannJson CACHE INTERNAL "SRC directory name of benchmark")
+    set(NLOHMANN_JSON_PATH ${NLOHMANN_JSON_OUTPUT}/${NLOHMANN_JSON_DIR} CACHE INTERNAL "Full path name to benchmark directory")
+
+    if(NOT EXISTS ${NLOHMANN_JSON_PATH}/nlohmann/json.hpp)
+        message(STATUS "download json header only to ${NLOHMANN_JSON_OUTPUT}")
+        file(MAKE_DIRECTORY ${NLOHMANN_JSON_PATH}/nlohmann)
+        file(DOWNLOAD https://github.com/nlohmann/json/releases/download/v3.9.1/json.hpp ${NLOHMANN_JSON_PATH}/nlohmann/json.hpp)
+
+    endif()
+    message(STATUS "use downloaded json header only")
+
+    add_library(nlohmann_json INTERFACE "")
+    target_include_directories(nlohmann_json INTERFACE ${NLOHMANN_JSON_PATH})
+
+    add_library(nlohmann_json::nlohmann_json ALIAS nlohmann_json)
+#    add_subdirectory(${BENCHMARK_PATH})
+endif ()
