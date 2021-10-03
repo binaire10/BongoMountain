6c4ae31a83fb509a10d55a90d2793890d100d185
Alexandre Wery <alexbwery@gmail.com>
2021-10-04 00:14:11 +0200

    split compilation between source

--- module/Graphic/src/Graphic/Module.cpp
+++ module/Graphic/src/Graphic/Module.cpp
@@ -1,15 +1,5 @@
 #include "Module.hpp"
 
-#ifdef BM_PLATFORM_GLFW
-#    include <GLFW/Module.cpp>
-#    include <GLFW/RenderSurface.cpp>
-#    include <GLFW/GraphicApplication.cpp>
-#endif
-#ifdef BM_PLATFORM_GLAD
-#    include <OpenGL/Module.cpp>
-#endif
-
-
 std::unique_ptr<GraphicModule> Graphic::createModule() { return std::make_unique<GraphicModule>(); }
 Graphic::RenderSurface *       Graphic::getMainSurface(std::unique_ptr<GraphicModule> &module)
 {
