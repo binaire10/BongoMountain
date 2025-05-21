e1cb4e00325dec302da6012c3db7b7c1731d0704
Alexandre Wery <alexbwery@gmail.com>
2025-05-21 21:50:50 +0200

    fixe calling convention

--- module/core/src/crypto/Crypto.cpp
+++ module/core/src/crypto/Crypto.cpp
@@ -5,7 +5,7 @@
 
 crypto::PKCS12_ptr crypto::loadPSCK12(std::string_view filename)
 {
-    return fixe_string(filename, [](auto filename){
+    return fixe_string([](std::string_view filename){
         // Charger la clé privée RSA à partir du fichier PKCS12
         FILE *p12File = fopen(filename.data(), "rb");// Ouvrir le fichier PKCS12 en mode binaire
         if(!p12File)
@@ -16,12 +16,12 @@ crypto::PKCS12_ptr crypto::loadPSCK12(std::string_view filename)
         PKCS12_ptr p12{ d2i_PKCS12_fp(p12File, nullptr) };// Lire le fichier PKCS12
         fclose(p12File);                                  // Fermer le fichier PKCS12
         return p12;
-    });
+    }, filename);
 }
 
 crypto::EVP_PKEY_ptr crypto::getPrivateKey(const crypto::PKCS12_ptr &p12, std::string_view password)
 {
-    return fixe_string(password, [&p12](auto password){
+    return fixe_string([&p12](std::string_view password){
         EVP_PKEY *pkey = nullptr;
         if(!PKCS12_parse(p12.get(), password.data(), &pkey, nullptr, nullptr))
         {                                  // Extraire la clé privée RSA sans mot de passe
@@ -29,5 +29,5 @@ crypto::EVP_PKEY_ptr crypto::getPrivateKey(const crypto::PKCS12_ptr &p12, std::s
             return EVP_PKEY_ptr{};// Retourner false en cas d'erreur
         }
         return EVP_PKEY_ptr{ pkey };
-    });
+    }, password);
 }
